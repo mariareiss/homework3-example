1e3a3fd3ddb8b03a3889f270c49ca53cd5d2ce1d
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-04 13:40:44 -0500

    Update view-professors-with-classes.php

--- view-professors-with-classes.php
+++ view-professors-with-classes.php
@@ -9,7 +9,7 @@ while ($professor = $professors-> fetch_assoc()) {
       <p class="card-text">
       <ul class="list-group">
 <?php
-  $Classes= selectClassByProfessor($professor['professor_id']);
+  $class= selectClassByProfessor($professor['professor_id']);
   while ($class=$Classes->fetch_assoc()){
 ?>
       <li class="list-group-item"><?php echo $class['class_number']; ?> - <?php echo $class['semester']; ?> - <?php echo $class['room']; ?> - <?php echo $class['day_time']; ?></li>
