81da3a7b660b3260eaa6fba08588a20de1e0046c
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-04 13:03:47 -0500

    Update professors-with-classes.php

--- professors-with-classes.php
+++ professors-with-classes.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-professors-with-classes.php");
 
+$pageTitle = "Professors with Classes";
+include "view-header.php";
+$professors = selectProfessors();
+include "view-professors-with-classes.php";
+include "view-footer.php";
+?>
