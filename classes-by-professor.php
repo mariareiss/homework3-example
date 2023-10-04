1d960ff79cd8852186f7d821b4fe1d11641af3ad
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-04 11:31:26 -0500

    Update classes-by-professor.php

--- classes-by-professor.php
+++ classes-by-professor.php
@@ -4,7 +4,7 @@ require_once("model-classes-by-professor.php");
 
 $pageTitle = "Classes by Professor";
 include "view-header.php";
-$Class = selectClassesByProfessor($_GET['id']);
+$Classes = selectClassesByProfessor($_GET['id']);
 include "view-classes-by-professor.php";
 include "view-footer.php";
 ?>
