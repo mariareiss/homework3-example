abd6b4cde00fced6a44ea7ecc2a59b8070591d32
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-04 13:08:43 -0500

    Update model-professors-with-classes.php

--- model-professors-with-classes.php
+++ model-professors-with-classes.php
@@ -12,4 +12,19 @@ function selectProfessors() {
         throw $e;
     }
 }
+
+function selectClassesByProfessor($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.class_id, class_number, class_description, semester, room, day_time FROM `class` c join section s on s.class_id = c.class_id where s.professor_id=?");
+       $stmt->bind_param("i", $pid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
