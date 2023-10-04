eaff4e3839b230e5a0e57cd706bd735eba4fd831
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-04 10:45:59 -0500

    Update view-classes.php

--- view-classes.php
+++ view-classes.php
@@ -1,1 +1,25 @@
-
+<h1>Classes</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Description</th> 
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($class = $classes-> fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $class['class_id']; ?></td>
+    <td><?php echo $class['class_number']; ?></td>
+    <td><?php echo $class['class_description']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
