0293f45dab673f8fa1ee35f0596615cf1d696692
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-04 11:49:15 -0500

    Update view-classes-by-professor.php

--- view-classes-by-professor.php
+++ view-classes-by-professor.php
@@ -3,29 +3,30 @@
   <table class="table">
     <thead>
       <tr>
-      <th>ID</th>
-      <th>Number</th>
-      <th>Description</th> 
-      <th>Semester</th>
-      <th>Room</th>
-      <th>Day/ Time</th>
+        <th>ID</th>
+        <th>Number</th>
+        <th>Description</th> 
+        <th>Semester</th>
+        <th>Room</th>
+        <th>Day/Time</th> 
       </tr>
     </thead>
     <tbody>
-<?php
-while ($class = $classes-> fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $class['class_id']; ?></td>
-    <td><?php echo $class['class_number']; ?></td>
-    <td><?php echo $class['class_description']; ?></td>
-    td><?php echo $class['semester']; ?></td>
-    td><?php echo $class['room']; ?></td>
-    td><?php echo $class['day_time']; ?></td>
-  </tr>
-<?php
-}
-?>
+      <?php
+      while ($class = $classes->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $class['class_id']; ?></td>
+          <td><?php echo $class['class_number']; ?></td>
+          <td><?php echo $class['class_description']; ?></td>
+          <td><?php echo $class['semester']; ?></td>
+          <td><?php echo $class['room']; ?></td>
+          <td><?php echo $class['day_time']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
     </tbody>
   </table>
 </div>
+
